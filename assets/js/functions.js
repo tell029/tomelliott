4d30c3c2813fb09d3df489e8f8680c6a4db8f8cf
tell029 <tell029@aucklanduni.ac.nz>
2015-08-08 22:31:14 +1200

    looks good on large/small dev; header fades in/out

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
+  // The mobile navigation toggle button
   $(".mobile-nav-toggle").click(function() {
     $(".mobile-nav-toggle").toggleClass("is-open");
     $(".mobile-nav").toggleClass("is-open");
+  });
+
+  // The title in the side/top-bar
+  var titleTop = $("#mainTitle").offset().top;
+  var titleHeight = $("#mainTitle").height();
+  $(window).scroll(function() {
+    if ( $(window).scrollTop() > titleTop + 0.8*titleHeight) {
+      $("#headerTitle").addClass("show");
+    } else {
+      $("#headerTitle").removeClass("show");
+    }
   });
 });
