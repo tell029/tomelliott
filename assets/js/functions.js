7f6fab513d81f85924c3ce26bd6a3a1bba12d2e4
Tom Elliott <tell029@aucklanduni.ac.nz>
2015-08-30 20:13:53 +1200

    research + projects section; better UI

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,18 +1,20 @@
 $(document).foundation();
 
 $(document).ready(function(){
-  headerFill();
+  headerStartup();
   navigationScrolling();
-  yearsSince();
   scrollEffects();
   researchInfo();
+  workWheel();
   resizeEvents();
 });
 
-headerFill = function() {
-  // $("#headerTitleFill").textfill({
-  //   maxFontPixels: 200
-  // });
+headerStartup = function() {
+  $('.header').waitForImages(function() {
+    setTimeout(function() {
+      $('.header').removeClass("uninitialised");
+    }, 500);
+  });
 }
 
 
@@ -28,20 +30,20 @@ navigationScrolling = function() {
   });
 }
 
-yearsSince = function() {
-  var year = (new Date).getFullYear();
-  var since = year - 2010;
-  $(".yearsSinceStarting").html(since);
-}
-
 researchInfo = function() {
   // resize to fit the content
   var maxHeight = -1;
-  $(".research-info .content").each(function() {
-    if ($(this).height() > maxHeight)
-      maxHeight = $(this).height();
-  });
-  $(".research-info").height(maxHeight);
+  var isMobile = Foundation.utils.is_small_only();
+
+  if (isMobile) {
+    $(".research-info").height($(".research-info .content.active").height());
+  } else {
+    $(".research-info .content").each(function() {
+      if ($(this).height() > maxHeight)
+        maxHeight = $(this).height();
+    });
+    $(".research-info").height(maxHeight);
+  }
 
   $(".research-topics .topic-meta").on("click", function() {
     var current = $(".research-info .content.active");
@@ -53,17 +55,65 @@ researchInfo = function() {
 
     current.addClass("blast-off");
 
-    $(".research-topics .active .show-for-touch").html("(touch to view)");
+    //$(".research-topics .active .show-for-touch").html("(touch to view)");
     $(".research-topics .active").removeClass("active");
 
     $(".research-topics ."+topic).addClass("active");
-    $(".research-topics .active .show-for-touch").html("(touch to hide)");
-    $(".research-topics .active .topic-meta").blur();
+    //$(".research-topics .active .show-for-touch").html("(touch to hide)");
+
+    if (isMobile) {
+      // As the new content comes in, scroll the viewport up ...
+      $("html,body").animate({
+        scrollTop: $(".research-info").offset().top
+      }, 500);
+
+      // ... and resize the div .research-info
+      $(".research-info").animate({
+        height: $(".research-info .content."+topic).height()
+      }, 500);
+    }
 
     setTimeout(function() {
       current.removeClass("active").removeClass("blast-off");
       $(".research-info .content."+topic).addClass("active");
     }, 300);
+  });
+}
+
+workWheel = function() {
+  // add .active to the first one
+  $(".work-wheel .work-item").first().addClass("active");
+  $(".work-content-wrap .work-content-item").first().addClass("active").removeClass("to-right").removeClass("to-left");
+
+  // get middle of .work-wrap
+  var workMiddle = $(".work-wrap").width() / 2;
+  $(".work-wheel").css('left', workMiddle);
+
+  // now, whenever an item is clicked, do something:
+  $(".work-item img").on("click", function() {
+    var $this = $(this),
+        $cont = $this.parent(),
+        fromLeft = - $cont.position().left,
+        newPos = workMiddle + fromLeft - $this.width()/2,
+        index = $(".work-wheel .work-item").index($cont),
+        oldIndex = $(".work-wheel .work-item").index($(".work-wheel .work-item.active"));
+
+    if (!$cont.hasClass("active")) {
+      $(".work-wheel .work-item.active").removeClass("active");
+      $(".work-wheel").css('left', newPos);
+
+      if (index > oldIndex) {
+        $(".work-content-item:eq("+oldIndex+")").removeClass("active").addClass("to-left");
+        // move in new text
+      } else {
+        $(".work-content-item:eq("+oldIndex+")").removeClass("active").addClass("to-right");
+      }
+
+      setTimeout(function() {
+        $cont.addClass("active");
+        $(".work-content-item:eq("+index+")").addClass("active").removeClass("to-left").removeClass("to-right");
+      }, 400);
+    }
   });
 }
 
@@ -86,6 +136,7 @@ scrollEffects = function() {
 
 resizeEvents = function() {
   $(window).resize(function(){
-
+    researchInfo();
+    workWheel();
   });
 }
