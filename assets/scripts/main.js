2f491a41667359c7c92cbd0aee077ecf21a4eeb9
Tom Elliott <tomelliottnz@gmail.com>
2017-03-16 07:12:44 +1300

    tidy up interactivity of research section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,15 @@
-// $(document).ready(function() {
-//     $("section").on("mouseover", function() {
-//         $(this).addClass('focused');
-//     });
-// });
+$(document).ready(function() {
+    $("#researchnav ul li").on("click", function() {
+        $this = $(this);
+
+        // remove class `active` from current, add to clicked
+        $("#researchnav ul li.active").removeClass("active");
+        $this.addClass("active");
+
+        // get index of clicked
+        $ind = $("#researchnav ul li").index($this);
+
+        // translate body
+        $("#researchItemContainer").css("transform", "translateX(-" + $ind * 100 + "%)");
+    });
+});
